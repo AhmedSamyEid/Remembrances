8661fd8adb257f13fe88db74d3d076519749dce3
Ahmed Samy <as8609377@gmail.com>
2025-01-17 01:53:23 +0200

    Added fetch.

--- assets/script/main.js
+++ assets/script/main.js
@@ -9,3 +9,26 @@ document.getElementById("crescent-moon").onclick = () => {
   }
 };
 
+document.getElementById("mnue").onclick = () => {
+  if (document.getElementById("nav-bar").classList.contains("opn")) {
+    document.getElementById("nav-bar").classList.remove("opn");
+  } else {
+    document.getElementById("nav-bar").classList.add("opn");
+  }
+};
+
+fetch(`https://www.myjsons.com/v/b7d03829`)
+  .then((res) => res.json())
+  .then((data) => {
+    let morning = "";
+    for (let i = 0; i < data["أذكار الصباح"].length; i++) {
+      morning += `
+        <li><span>${data["أذكار الصباح"][i].content}</span></li>
+          
+      `;
+    }
+    document.getElementById("morning-List").innerHTML = morning;
+  });
+
+
+  
