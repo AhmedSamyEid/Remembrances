b9808682aa6e7ddb0441594eb3b92e9772e8b439
Ahmed Samy <as8609377@gmail.com>
2025-01-17 01:52:15 +0200

    Night mode has been added.

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,1 +1,11 @@
+document.getElementById("crescent-moon").onclick = () => {
+  if (document.body.classList.contains("crescent-moon")) {
+    document.body.classList.remove("crescent-moon");
+    document.getElementById("crescent-moon").src =
+      "assets/icons/crescent moon.webp";
+  } else {
+    document.body.classList.add("crescent-moon");
+    document.getElementById("crescent-moon").src = "assets/icons/sun.webp";
+  }
+};
 
